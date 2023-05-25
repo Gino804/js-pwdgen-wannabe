03d4b709b453cbd21ded1f731cdbd1fcc415c914
Gino804 <giacomo.p03@gmail.com>
2023-05-25 16:04:46 +0200

    Added Javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 console.log('JS OK');
+
+// OPERAZIONI PRELIMINARI
+
+// Recupero l'elemento dal DOM
+
+const passwordPlaceholder = document.getElementById('generated-password');
+console.log(passwordPlaceholder);
+
+// Chiedo il nome all'utente e lo salvo in una variabile
+
+const userName = prompt('Inserisci il tuo nome','Ezio');
+console.log(userName);
+
+// Chiedo il cognome all'utente e lo salvo in una variabile
+
+const userLastname = prompt('Inserisci il tuo cognome','Greggio');
+console.log(userLastname);
+
+// Chiedo il colore preferito all'utente e lo salvo in una variabile
+
+const userColor = prompt('Inserisci il tuo colore preferito','rosa');
+console.log(userColor);
+
+// Calcolo la password completa
+
+const completePassword = userName + userLastname + userColor + 21; // Creare una variabile apposita per un semplice numero che non ha altra funzione all'infuori di questa mi sembrava superfluo, quindi l'ho inserito direttamente nell'operazione
+
+console.log(completePassword);
+
+// Inserisco la password generata nell'elemento del DOM
+
+passwordPlaceholder.innerText = completePassword;
